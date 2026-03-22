d1093af2fc6096e5ac53a0197527c2b073d317f7
hAze4561 <maksimskripnik88@gmail.com>
2026-03-22 10:26:08 +0300

    Add files via upload

---
+++ Labs/Labs10/PoleChudes/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace PoleChudes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnProverka = new System.Windows.Forms.Button();
+            this.btnOtmena = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSlovo = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(12, 24);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(128, 29);
+            this.btnNew.TabIndex = 0;
+            this.btnNew.Text = "Новая игра";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click_1);
+            // 
+            // btnProverka
+            // 
+            this.btnProverka.Location = new System.Drawing.Point(146, 24);
+            this.btnProverka.Name = "btnProverka";
+            this.btnProverka.Size = new System.Drawing.Size(128, 29);
+            this.btnProverka.TabIndex = 1;
+            this.btnProverka.Text = "Проверить";
+            this.btnProverka.UseVisualStyleBackColor = true;
+            this.btnProverka.Click += new System.EventHandler(this.btnProverka_Click_1);
+            // 
+            // btnOtmena
+            // 
+            this.btnOtmena.Location = new System.Drawing.Point(280, 24);
+            this.btnOtmena.Name = "btnOtmena";
+            this.btnOtmena.Size = new System.Drawing.Size(128, 29);
+            this.btnOtmena.TabIndex = 2;
+            this.btnOtmena.Text = "Отмена";
+            this.btnOtmena.UseVisualStyleBackColor = true;
+            this.btnOtmena.Click += new System.EventHandler(this.btnOtmena_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Собираемое";
+            // 
+            // txtSlovo
+            // 
+            this.txtSlovo.Location = new System.Drawing.Point(155, 83);
+            this.txtSlovo.Name = "txtSlovo";
+            this.txtSlovo.Size = new System.Drawing.Size(194, 22);
+            this.txtSlovo.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn8);
+            this.groupBox1.Controls.Add(this.btn10);
+            this.groupBox1.Controls.Add(this.btn9);
+            this.groupBox1.Controls.Add(this.btn7);
+            this.groupBox1.Controls.Add(this.btn6);
+            this.groupBox1.Controls.Add(this.btn5);
+            this.groupBox1.Controls.Add(this.btn3);
+            this.groupBox1.Controls.Add(this.btn4);
+            this.groupBox1.Controls.Add(this.btn2);
+            this.groupBox1.Controls.Add(this.btn1);
+            this.groupBox1.Location = new System.Drawing.Point(17, 132);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(759, 294);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(524, 121);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(59, 48);
+            this.btn8.TabIndex = 9;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Visible = false;
+            // 
+            // btn10
+            // 
+            this.btn10.Location = new System.Drawing.Point(688, 121);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(59, 48);
+            this.btn10.TabIndex = 8;
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Visible = false;
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(606, 121);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(59, 48);
+            this.btn9.TabIndex = 7;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Visible = false;
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(445, 121);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(59, 48);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Visible = false;
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(367, 121);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(59, 48);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Visible = false;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(286, 121);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(59, 48);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Visible = false;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(142, 121);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(59, 48);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Visible = false;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(214, 121);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(59, 48);
+            this.btn4.TabIndex = 2;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Visible = false;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(75, 121);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(59, 48);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Visible = false;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(6, 121);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(59, 48);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtSlovo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOtmena);
+            this.Controls.Add(this.btnProverka);
+            this.Controls.Add(this.btnNew);
+            this.Name = "Form1";
+            this.Text = "Поле чудес";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnProverka;
+        private System.Windows.Forms.Button btnOtmena;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSlovo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ Labs/Labs10/PoleChudes/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PoleChudes
+{
+    public partial class Form1 : Form
+    {
+        // Подключение к базе данных
+        private string connectionString = @"Data Source=HAZE\SQLEXPRESS;Initial Catalog=PoleChudesDB;Integrated Security=True";
+
+        // Переменные игры
+        private string currentWord; // Загаданное слово
+        private string shuffledWord; // Перемешанное слово
+        private List<Button> letterButtons; // Список кнопок с буквами
+        private List<string> userLetters; // Собранные пользователем буквы
+        private Stack<Tuple<int, string>> undoStack; // Стек для отмены действий
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            // Инициализация списков
+            letterButtons = new List<Button>();
+            userLetters = new List<string>();
+            undoStack = new Stack<Tuple<int, string>>();
+
+            // Добавляем все кнопки с буквами в список
+            letterButtons.Add(btn1);
+            letterButtons.Add(btn2);
+            letterButtons.Add(btn3);
+            letterButtons.Add(btn4);
+            letterButtons.Add(btn5);
+            letterButtons.Add(btn6);
+            letterButtons.Add(btn7);
+            letterButtons.Add(btn8);
+            letterButtons.Add(btn9);
+            letterButtons.Add(btn10);
+
+            // Подписываем все кнопки на одно событие
+            foreach (Button btn in letterButtons)
+            {
+                btn.Click += LetterButton_Click;
+            }
+
+            // Очищаем текстовое поле
+            txtSlovo.Clear();
+            txtSlovo.ReadOnly = true;
+
+            // Делаем кнопку "Проверить" доступной
+            btnProverka.Enabled = true;
+        }
+
+        // Метод для получения случайного слова из базы данных
+        private string GetRandomWordFromDB()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT TOP 1 Word FROM Words ORDER BY NEWID()";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    object result = command.ExecuteScalar();
+
+                    if (result != null)
+                    {
+                        return result.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("В базе данных нет слов!", "Ошибка",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка подключения к базе данных: {ex.Message}",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        // Метод для перемешивания букв
+        private string ShuffleWord(string word)
+        {
+            char[] array = word.ToCharArray();
+            Random rng = new Random();
+
+            // Алгоритм Фишера-Йетса для перемешивания
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int j = rng.Next(0, i + 1);
+                char temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+
+            return new string(array);
+        }
+
+        // Метод для обновления интерфейса (отображения букв на кнопках)
+        private void UpdateInterface()
+        {
+            // Скрываем все кнопки
+            foreach (Button btn in letterButtons)
+            {
+                btn.Visible = false;
+            }
+
+            // Отображаем кнопки с буквами
+            for (int i = 0; i < shuffledWord.Length; i++)
+            {
+                if (i < letterButtons.Count)
+                {
+                    letterButtons[i].Text = shuffledWord[i].ToString();
+                    letterButtons[i].Visible = true;
+                    letterButtons[i].Enabled = true;
+                }
+            }
+
+            // Обновляем текстовое поле с собранным словом
+            txtSlovo.Text = string.Join("", userLetters);
+
+            // Визуально показываем длину загаданного слова
+            this.Text = $"Поле Чудес - Загадано слово из {currentWord.Length} букв";
+        }
+
+        // Обработчик нажатия на букву
+        private void LetterButton_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            if (clickedButton != null && clickedButton.Enabled)
+            {
+                // Сохраняем действие для отмены
+                int currentPosition = userLetters.Count;
+                string letter = clickedButton.Text;
+                undoStack.Push(new Tuple<int, string>(currentPosition, letter));
+
+                // Добавляем букву к собранному слову
+                userLetters.Add(letter);
+
+                // Делаем кнопку недоступной
+                clickedButton.Enabled = false;
+
+                // Обновляем интерфейс
+                UpdateInterface();
+            }
+        }
+
+        private void btnNew_Click_1(object sender, EventArgs e)
+        {
+            currentWord = GetRandomWordFromDB();
+
+            if (string.IsNullOrEmpty(currentWord))
+            {
+                return;
+            }
+
+            // Приводим к верхнему регистру и заменяем Ё на Е
+            currentWord = currentWord.ToUpper();
+            currentWord = currentWord.Replace('Ё', 'Е');
+
+            // Перемешиваем буквы
+            shuffledWord = ShuffleWord(currentWord);
+
+            // Очищаем собранные буквы
+            userLetters.Clear();
+
+            // Очищаем стек отмены
+            undoStack.Clear();
+
+            // Обновляем интерфейс
+            UpdateInterface();
+        }
+
+        private void btnProverka_Click_1(object sender, EventArgs e)
+        {
+            string userWord = string.Join("", userLetters);
+
+            if (string.IsNullOrEmpty(userWord))
+            {
+                MessageBox.Show("Вы еще не выбрали ни одной буквы!",
+                    "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Проверяем, совпадает ли собранное слово с загаданным
+            if (userWord.Equals(currentWord, StringComparison.OrdinalIgnoreCase))
+            {
+                MessageBox.Show($"Поздравляю! Вы угадали слово: {currentWord}",
+                    "Победа!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show($"Неправильно! Загаданное слово: {currentWord}",
+                    "Результат", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnOtmena_Click_1(object sender, EventArgs e)
+        {
+            if (undoStack.Count > 0)
+            {
+                // Получаем последнее действие
+                var lastAction = undoStack.Pop();
+                int position = lastAction.Item1;
+                string letter = lastAction.Item2;
+
+                // Удаляем последнюю букву из собранного слова
+                if (userLetters.Count > 0)
+                {
+                    userLetters.RemoveAt(userLetters.Count - 1);
+                }
+
+                // Находим и активируем кнопку с этой буквой
+                foreach (Button btn in letterButtons)
+                {
+                    if (btn.Text == letter && !btn.Enabled && btn.Visible)
+                    {
+                        btn.Enabled = true;
+                        break;
+                    }
+                }
+
+                // Обновляем интерфейс
+                UpdateInterface();
+            }
+            else
+            {
+                MessageBox.Show("Нет действий для отмены!",
+                    "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
