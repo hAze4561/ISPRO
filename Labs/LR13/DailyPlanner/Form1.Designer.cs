c22bbeaeb847859dc1493d748dc83c33c6040500
hAze4561 <maksimskripnik88@gmail.com>
2026-04-04 16:31:40 +0300

    Add files via upload

---
+++ Labs/LR13/DailyPlanner/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace DailyPlanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listNotes = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtNoteText = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.monthCalendar1);
+            this.groupBox1.Location = new System.Drawing.Point(10, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 267);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор даты";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(12, 48);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listNotes);
+            this.groupBox2.Location = new System.Drawing.Point(247, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(547, 266);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Список заметок";
+            // 
+            // listNotes
+            // 
+            this.listNotes.FormattingEnabled = true;
+            this.listNotes.ItemHeight = 16;
+            this.listNotes.Location = new System.Drawing.Point(10, 24);
+            this.listNotes.Name = "listNotes";
+            this.listNotes.Size = new System.Drawing.Size(536, 228);
+            this.listNotes.TabIndex = 0;
+            this.listNotes.SelectedIndexChanged += new System.EventHandler(this.listNotes_SelectedIndexChanged_1);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnDelete);
+            this.groupBox3.Controls.Add(this.btnClear);
+            this.groupBox3.Controls.Add(this.btnEdit);
+            this.groupBox3.Controls.Add(this.btnAdd);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.dateTimePicker1);
+            this.groupBox3.Controls.Add(this.txtNoteText);
+            this.groupBox3.Location = new System.Drawing.Point(10, 296);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(782, 208);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Добавление и редактирование заметки";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(484, 176);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(151, 26);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click_1);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(327, 176);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(151, 26);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click_1);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(170, 176);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(151, 26);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Редактировать";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click_1);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(13, 176);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(151, 26);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выберите время";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(10, 137);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(91, 22);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.Value = new System.DateTime(2026, 4, 4, 15, 45, 0, 0);
+            // 
+            // txtNoteText
+            // 
+            this.txtNoteText.Location = new System.Drawing.Point(9, 27);
+            this.txtNoteText.Multiline = true;
+            this.txtNoteText.Name = "txtNoteText";
+            this.txtNoteText.Size = new System.Drawing.Size(753, 95);
+            this.txtNoteText.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 516);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Заметки с календарем";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listNotes;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txtNoteText;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnEdit;
+    }
+}
+
---
+++ Labs/LR13/DailyPlanner/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace DailyPlanner
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Server=HAZE\SQLEXPRESS;Database=DailyPlannerDB;Trusted_Connection=True;";
+        private int? selectedNoteId = null; // ID выбранной заметки
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Настройка DateTimePicker для отображения только времени
+            dateTimePicker1.Format = DateTimePickerFormat.Time;
+            dateTimePicker1.ShowUpDown = true;
+
+            // Загрузка заметок на сегодняшнюю дату
+            LoadNotes();
+        }
+
+        // Загрузка заметок на выбранную дату
+        private void LoadNotes()
+        {
+            DateTime selectedDate = monthCalendar1.SelectionStart.Date;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"SELECT Id, NoteDate, NoteText 
+                                   FROM Notes 
+                                   WHERE CAST(NoteDate AS DATE) = @SelectedDate 
+                                   ORDER BY NoteDate ASC";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@SelectedDate", selectedDate);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    listNotes.Items.Clear();
+
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32(0);
+                        DateTime noteDate = reader.GetDateTime(1);
+                        string noteText = reader.GetString(2);
+
+                        string displayText = $"{noteDate:HH:mm} - {TruncateText(noteText, 60)}";
+
+                        ListBoxItem item = new ListBoxItem
+                        {
+                            Id = id,
+                            DisplayText = displayText,
+                            FullText = noteText,
+                            NoteDateTime = noteDate
+                        };
+
+                        listNotes.Items.Add(item);
+                    }
+
+                    reader.Close();
+
+                    if (listNotes.Items.Count == 0)
+                    {
+                        listNotes.Items.Add("Нет заметок на этот день");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка подключения к базе данных: {ex.Message}", "Ошибка",
+                                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Обрезка длинного текста
+        private string TruncateText(string text, int maxLength)
+        {
+            if (text.Length <= maxLength)
+                return text;
+            return text.Substring(0, maxLength) + "...";
+        }
+
+        private void btnAdd_Click_1(object sender, EventArgs e)
+        {
+            string noteText = txtNoteText.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(noteText))
+            {
+                MessageBox.Show("Введите текст заметки!", "Предупреждение",
+                               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DateTime selectedDate = monthCalendar1.SelectionStart.Date;
+            DateTime selectedTime = dateTimePicker1.Value;
+            DateTime fullDateTime = selectedDate.Add(selectedTime.TimeOfDay);
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"INSERT INTO Notes (NoteDate, NoteText, CreatedAt) 
+                                   VALUES (@NoteDate, @NoteText, GETDATE())";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@NoteDate", fullDateTime);
+                    cmd.Parameters.AddWithValue("@NoteText", noteText);
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Заметка успешно добавлена!", "Успех",
+                                   MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    txtNoteText.Clear();
+                    dateTimePicker1.Value = DateTime.Now;
+                    selectedNoteId = null;
+                    LoadNotes();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при добавлении: {ex.Message}", "Ошибка",
+                                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void listNotes_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (listNotes.SelectedItem is ListBoxItem item && item.Id > 0)
+            {
+                selectedNoteId = item.Id;
+                txtNoteText.Text = item.FullText;
+                dateTimePicker1.Value = item.NoteDateTime;
+            }
+        }
+
+        private void btnEdit_Click_1(object sender, EventArgs e)
+        {
+            if (selectedNoteId == null)
+            {
+                MessageBox.Show("Сначала выберите заметку для редактирования!", "Предупреждение",
+                               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string noteText = txtNoteText.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(noteText))
+            {
+                MessageBox.Show("Введите текст заметки!", "Предупреждение",
+                               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DateTime selectedDate = monthCalendar1.SelectionStart.Date;
+            DateTime selectedTime = dateTimePicker1.Value;
+            DateTime fullDateTime = selectedDate.Add(selectedTime.TimeOfDay);
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"UPDATE Notes 
+                                   SET NoteDate = @NoteDate, NoteText = @NoteText, CreatedAt = GETDATE() 
+                                   WHERE Id = @Id";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@NoteDate", fullDateTime);
+                    cmd.Parameters.AddWithValue("@NoteText", noteText);
+                    cmd.Parameters.AddWithValue("@Id", selectedNoteId.Value);
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Заметка успешно обновлена!", "Успех",
+                                   MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    txtNoteText.Clear();
+                    dateTimePicker1.Value = DateTime.Now;
+                    selectedNoteId = null;
+                    LoadNotes();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при обновлении: {ex.Message}", "Ошибка",
+                                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnClear_Click_1(object sender, EventArgs e)
+        {
+            txtNoteText.Clear();
+            dateTimePicker1.Value = DateTime.Now;
+            selectedNoteId = null;
+        }
+
+        private void btnDelete_Click_1(object sender, EventArgs e)
+        {
+            if (selectedNoteId == null)
+            {
+                MessageBox.Show("Выберите заметку для удаления!", "Предупреждение",
+                               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult result = MessageBox.Show("Вы уверены, что хотите удалить эту заметку?",
+                                                  "Подтверждение удаления",
+                                                  MessageBoxButtons.YesNo,
+                                                  MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        string query = "DELETE FROM Notes WHERE Id = @Id";
+
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("@Id", selectedNoteId.Value);
+
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("Заметка удалена!", "Успех",
+                                       MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        txtNoteText.Clear();
+                        dateTimePicker1.Value = DateTime.Now;
+                        selectedNoteId = null;
+                        LoadNotes();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка при удалении: {ex.Message}", "Ошибка",
+                                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            LoadNotes();
+        }
+    }
+
+    // Вспомогательный класс для хранения данных заметки в ListBox
+    public class ListBoxItem
+    {
+        public int Id { get; set; }
+        public string DisplayText { get; set; }
+        public string FullText { get; set; }
+        public DateTime NoteDateTime { get; set; }
+
+        public override string ToString()
+        {
+            return DisplayText;
+        }
+    }
+}
