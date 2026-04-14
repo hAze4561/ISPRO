b82daf8e7c3e42ba7228e391a4fa7de6d7d1b2b6
hAze4561 <maksimskripnik88@gmail.com>
2026-04-14 18:16:30 +0300

    Add files via upload

---
+++ Labs/LR14/NumberSystemConverter/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace NumberSystemConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnKonvert = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(658, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Поддерживаемые системы: двоичная (2), восьмеричная (8), десятичная (10), шестнадц" +
+    "атеричная (16)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Из системы:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "В систему:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Число:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Результат:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(105, 80);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(190, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(105, 112);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(190, 24);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 143);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(105, 188);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // btnKonvert
+            // 
+            this.btnKonvert.Location = new System.Drawing.Point(45, 224);
+            this.btnKonvert.Name = "btnKonvert";
+            this.btnKonvert.Size = new System.Drawing.Size(153, 29);
+            this.btnKonvert.TabIndex = 9;
+            this.btnKonvert.Text = "Конвертировать";
+            this.btnKonvert.UseVisualStyleBackColor = true;
+            this.btnKonvert.Click += new System.EventHandler(this.btnKonvert_Click_1);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(217, 227);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(152, 25);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 270);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(154, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "История конвертаций:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 305);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(777, 198);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 510);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnKonvert);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Конвертер систем счисления";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnKonvert;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Labs/LR14/NumberSystemConverter/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace NumberSystemConverter
+{
+    public partial class Form1 : Form
+    {
+        // Строка подключения к БД (HAZE\SQLEXPRESS)
+        private string connectionString = @"Server=HAZE\SQLEXPRESS;Database=NumberSystemsDB;Integrated Security=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComboBoxes();
+            LoadHistory();
+        }
+
+        // Заполнение ComboBox вариантами систем счисления
+        private void InitializeComboBoxes()
+        {
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+
+            comboBox1.Items.Add("Двоичная (2)");
+            comboBox1.Items.Add("Восьмеричная (8)");
+            comboBox1.Items.Add("Десятичная (10)");
+            comboBox1.Items.Add("Шестнадцатеричная (16)");
+
+            comboBox2.Items.Add("Двоичная (2)");
+            comboBox2.Items.Add("Восьмеричная (8)");
+            comboBox2.Items.Add("Десятичная (10)");
+            comboBox2.Items.Add("Шестнадцатеричная (16)");
+
+            comboBox1.SelectedIndex = 2; // Десятичная по умолчанию
+            comboBox2.SelectedIndex = 2;
+        }
+
+        // Валидация ввода в зависимости от выбранной системы
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null) return;
+
+            string selectedSystem = comboBox1.SelectedItem.ToString();
+            string input = textBox1.Text;
+            string pattern = GetValidationPattern(selectedSystem);
+
+            if (!string.IsNullOrEmpty(input) && !Regex.IsMatch(input, pattern, RegexOptions.IgnoreCase))
+            {
+                // Удаляем последний недопустимый символ
+                textBox1.Text = input.Substring(0, input.Length - 1);
+                textBox1.SelectionStart = textBox1.Text.Length;
+            }
+        }
+
+        // Получение регулярного выражения для валидации
+        private string GetValidationPattern(string system)
+        {
+            if (system.Contains("Двоичная"))
+                return "^[01]*$";
+            if (system.Contains("Восьмеричная"))
+                return "^[0-7]*$";
+            if (system.Contains("Десятичная"))
+                return "^[0-9]*$";
+            if (system.Contains("Шестнадцатеричная"))
+                return "^[0-9A-Fa-f]*$";
+            return "^.*$";
+        }
+
+        // Получение основания системы из строки ComboBox
+        private int GetBaseFromComboBox(string systemString)
+        {
+            if (systemString.Contains("Двоичная")) return 2;
+            if (systemString.Contains("Восьмеричная")) return 8;
+            if (systemString.Contains("Десятичная")) return 10;
+            if (systemString.Contains("Шестнадцатеричная")) return 16;
+            return 10;
+        }
+
+        // Преобразование числа в десятичную систему (схема Горнера)
+        private long ConvertToDecimal(string number, int fromBase)
+        {
+            if (fromBase == 10)
+                return long.Parse(number);
+
+            long result = 0;
+            number = number.ToUpper();
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                int digitValue = GetDigitValue(number[i]);
+                if (digitValue >= fromBase)
+                    throw new Exception($"Недопустимая цифра '{number[i]}' для системы с основанием {fromBase}");
+
+                result = result * fromBase + digitValue;
+            }
+            return result;
+        }
+
+        // Получение числового значения цифры/буквы
+        private int GetDigitValue(char digit)
+        {
+            if (digit >= '0' && digit <= '9')
+                return digit - '0';
+            if (digit >= 'A' && digit <= 'F')
+                return 10 + (digit - 'A');
+            throw new Exception($"Недопустимый символ '{digit}'");
+        }
+
+        // Преобразование из десятичной в целевую систему
+        private string ConvertFromDecimal(long decimalValue, int toBase)
+        {
+            if (toBase == 10)
+                return decimalValue.ToString();
+
+            if (decimalValue == 0)
+                return "0";
+
+            StringBuilder result = new StringBuilder();
+            long temp = decimalValue;
+
+            while (temp > 0)
+            {
+                long remainder = temp % toBase;
+                result.Insert(0, GetDigitChar(remainder));
+                temp /= toBase;
+            }
+
+            return result.ToString();
+        }
+
+        // Получение символа для числового значения (для систем >10)
+        private string GetDigitChar(long value)
+        {
+            if (value < 10)
+                return value.ToString();
+            return ((char)('A' + (value - 10))).ToString();
+        }
+
+     
+        private void LoadHistory()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = @"SELECT Id, InputNumber, InputBase, OutputNumber, OutputBase, ConversionDate 
+                                    FROM ConversionHistory 
+                                    ORDER BY ConversionDate DESC";
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    // Настройка отображения колонок
+                    dataGridView1.DataSource = dataTable;
+
+                    // Переименование колонок для удобства
+                    if (dataGridView1.Columns.Contains("Id"))
+                        dataGridView1.Columns["Id"].HeaderText = "ID";
+                    if (dataGridView1.Columns.Contains("InputNumber"))
+                        dataGridView1.Columns["InputNumber"].HeaderText = "Исходное число";
+                    if (dataGridView1.Columns.Contains("InputBase"))
+                        dataGridView1.Columns["InputBase"].HeaderText = "Из системы";
+                    if (dataGridView1.Columns.Contains("OutputNumber"))
+                        dataGridView1.Columns["OutputNumber"].HeaderText = "Результат";
+                    if (dataGridView1.Columns.Contains("OutputBase"))
+                        dataGridView1.Columns["OutputBase"].HeaderText = "В систему";
+                    if (dataGridView1.Columns.Contains("ConversionDate"))
+                        dataGridView1.Columns["ConversionDate"].HeaderText = "Дата конвертации";
+
+                    // Форматирование даты
+                    dataGridView1.Columns["ConversionDate"].DefaultCellStyle.Format = "dd.MM.yyyy HH:mm:ss";
+                }
+            }
+            catch (SqlException ex)
+            {
+                HandleDatabaseError(ex);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки истории: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Обработка ошибок базы данных
+        private void HandleDatabaseError(SqlException ex)
+        {
+            string errorMessage = "Ошибка при работе с базой данных:\n";
+
+            switch (ex.Number)
+            {
+                case 53: // Network-related error
+                case 2:  // Server not found
+                    errorMessage += "Отсутствует подключение к серверу базы данных.\n";
+                    errorMessage += "Проверьте, запущен ли SQL Server и правильно ли указан сервер (HAZE\\SQLEXPRESS).";
+                    break;
+                case 4060: // Database not found
+                    errorMessage += "База данных 'NumberSystemsDB' не найдена.\n";
+                    errorMessage += "Создайте базу данных и таблицу ConversionHistory.";
+                    break;
+                case 208: // Invalid object name
+                    errorMessage += "Таблица 'ConversionHistory' не существует.\n";
+                    errorMessage += "Создайте таблицу в базе данных.";
+                    break;
+                default:
+                    errorMessage += ex.Message;
+                    break;
+            }
+
+            MessageBox.Show(errorMessage, "Ошибка базы данных", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btnKonvert_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    MessageBox.Show("Введите число для конвертации!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                string inputNumber = textBox1.Text.Trim();
+                int fromBase = GetBaseFromComboBox(comboBox1.SelectedItem.ToString());
+                int toBase = GetBaseFromComboBox(comboBox2.SelectedItem.ToString());
+
+                // Конвертация в десятичную систему
+                long decimalValue = ConvertToDecimal(inputNumber, fromBase);
+
+                // Конвертация из десятичной в целевую систему
+                string result = ConvertFromDecimal(decimalValue, toBase);
+
+                textBox2.Text = result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка конвертации: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSave_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Сначала выполните конвертацию!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = @"INSERT INTO ConversionHistory 
+                                    (InputNumber, InputBase, OutputNumber, OutputBase, ConversionDate) 
+                                    VALUES (@InputNumber, @InputBase, @OutputNumber, @OutputBase, @ConversionDate)";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@InputNumber", textBox1.Text.Trim());
+                        command.Parameters.AddWithValue("@InputBase", GetBaseFromComboBox(comboBox1.SelectedItem.ToString()));
+                        command.Parameters.AddWithValue("@OutputNumber", textBox2.Text.Trim());
+                        command.Parameters.AddWithValue("@OutputBase", GetBaseFromComboBox(comboBox2.SelectedItem.ToString()));
+                        command.Parameters.AddWithValue("@ConversionDate", DateTime.Now);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show("Конвертация успешно сохранена!", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadHistory(); // Обновляем историю
+            }
+            catch (SqlException ex)
+            {
+                HandleDatabaseError(ex);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
